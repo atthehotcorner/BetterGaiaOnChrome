1a90f8884f1f6c0bfcfb9037764fa55d5f46b864
atthehotcorner <at.thehotcorner@gmail.com>
2014-01-19 04:31:33 -0500

    More

--- code/js/main.js
+++ code/js/main.js
@@ -21,3 +21,50 @@ $('#gaia_header .userName').append('<ul id="bg_widgets">\
 
 // Shortcuts
 $('#gaia_header .userName').append('<ul id="bg_shortcuts"><a>Shortcuts</a></ul>');
+
+// Private Messages
+if (/*pref['main.features.messages'] == true && */document.location.pathname.indexOf('/profile/privmsg.php') > -1) {
+	// Private message selectors
+	$('body.mail #pm_content table tr[height="20"] td[align]').after("<div class='bg_selecttypes'><span><a class='all'>All</a><a class='read'>Read</a><a  class='unread'>Unread</a><a class='replied'>Replied</a><a class='none'>None</a></span></div>");	
+	$('body.mail #pm_content table tr[bgcolor][height="42"] > td:nth-of-type(2) img').each(function(index, element) {
+		$(this).closest('tr[bgcolor][height="42"]').attr('status', $(this).attr('title'));
+  });
+
+	$('body.mail #pm_content .bg_selecttypes a').on('click', function() {
+		if ($(this).hasClass('all')) $('tr[bgcolor][height="42"]:not(.bgpm_hide) input[type="checkbox"]').prop('checked', true);
+		if ($(this).hasClass('read')) $('tr[bgcolor][height="42"][status="Read Message"]:not(.bgpm_hide) input[type="checkbox"]').prop('checked', true);
+		if ($(this).hasClass('unread')) $('tr[bgcolor][height="42"][status="Unread Message"]:not(.bgpm_hide) input[type="checkbox"]').prop('checked', true);
+		if ($(this).hasClass('replied')) $('tr[bgcolor][height="42"][status="Replied Message"]:not(.bgpm_hide) input[type="checkbox"]').prop('checked', true);
+		if ($(this).hasClass('none')) $('tr[bgcolor][height="42"]:not(.bgpm_hide) input[type="checkbox"]').prop('checked', false);
+	});
+
+	// Add Avatar Image
+	var userids = [];
+	$('tr[bgcolor][height="42"] span.name a[href]').each(function(index, element) {
+		var userid = $(this).attr("href").split("/")[5];
+		$(this).closest('tr[bgcolor][height="42"]').attr("userid", userid);
+
+		if (userids.indexOf(userid) == -1) {
+			userids.push(userid);
+			$.get("/profiles?mode=lookup&avatar_username=" + $(this).closest('tr[bgcolor][height="42"]').find('span.name').text(), function(data){
+				var i = document.createElement('img');
+				$(i).load(function(){
+       		$('tr[bgcolor][height="42"][userid="'+userid+'"] span.topictitle a').css({'background-image': 'url(/dress-up/avatar/' + $(data).find('response').attr('avatarPath') + ')', 'background-position': 'right -35px'});
+				});
+				i.src = 'http://www.gaiaonline.com/dress-up/avatar/' + $(data).find('response').attr('avatarPath');
+			});
+		}
+	});
+
+	// Instant Search
+	$('body.mail #pm_content table tr[height="20"]').append('<input type="text" class="bgpm_search" placeholder="Search" value="" />');
+	$.extend($.expr[":"], {"Contains": function(elem, i, match, array) {return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;}});
+	$('body.mail #pm_content table tr[height="20"] .bgpm_search').keyup(function() {
+		var value = $(this).val();
+		if (value.length > 0) {
+			$('body.mail #pm_content table tr[bgcolor][height="42"]:not(:Contains("' + value + '"))').addClass('bgpm_hide').find('input[type="checkbox"]').prop('checked', false);
+			$('body.mail #pm_content table tr[bgcolor][height="42"]:Contains("' + value + '")').removeClass('bgpm_hide');
+		}
+		else $('body.mail #pm_content table tr[bgcolor][height="42"]').removeClass('bgpm_hide');
+	});
+}
