3dcc9566656ce8e91c93e1eda84d58c14f58d9ad
atthehotcorner <at.thehotcorner@gmail.com>
2014-01-19 03:28:35 -0500

    Adding more

---
+++ code/js/main.js
@@ -0,0 +1,23 @@
+// Widgets
+$('#gaia_header .userName').append('<ul id="bg_widgets">\
+    <li class="bgfriends">\
+        <a></a>\
+        <div></div>\
+    </li>\
+    <li class="bgmessages">\
+        <a></a>\
+        <div></div>\
+    </li>\
+    <li class="bgthreads">\
+        <a></a>\
+        <div></div>\
+    </li>\
+    <li class="bgwatchlist">\
+        <a></a>\
+        <div></div>\
+    </li>\
+</ul>');
+
+
+// Shortcuts
+$('#gaia_header .userName').append('<ul id="bg_shortcuts"><a>Shortcuts</a></ul>');
