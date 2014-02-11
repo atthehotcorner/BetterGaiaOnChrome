ffa6ba97f72d091e9019f04a786cc5fbb9ded957
atthehotcorner <at.thehotcorner@gmail.com>
2014-02-11 02:24:59 -0500

    new sortable, styles

--- settings/main.js
+++ settings/main.js
@@ -13,6 +13,11 @@ function Main() {
 
         $('header menu a.current').removeClass('current');
         $(this).addClass('current');
+
+        if (tabClass == 'about')
+        chrome.storage.sync.getBytesInUse(function(data){
+            $('page.about strong.inuse').text(data);
+        });
 		});
 
     if (window.location.hash) $('header menu a[href="' + window.location.hash + '"]').click();
@@ -39,13 +44,28 @@ function Main() {
         else $(this).closest('page').removeClass('off');
     });
 
+
+    // Enable format sorting
+    function formatSorting() {
+        $('#postformating aside').sortable({items: 'format:not(.add)'}).on('sortupdate', function(){
+            //Triggered when the user stopped sorting and the DOM position has changed.
+            console.log('done dragging!');
+        });
+    }
+
     // Insert formats
     $.each(prefs['format.list'], function(key, format) {
-        $('#postformating aside format.add').before('<format>' + key + '</format>');
+        $('#postformating aside format.add').before('<format data-bbcode="' + format[0] + '" data-poststyle="' + format[1] + '"><strong>' + key + '</strong>\
+        <div class="clear"><a class="edit">Edit</a><a class="delete">Delete</a></div></format>');
     });
+    formatSorting();
     
     $('#postformating aside format.add').on('click', function(){
-        $(this).before('<format>Kat</format>');
+        var bbcode = "%5Bcolor=#003040%5D%E2%96%8C%5B/color%5D%5Bb%5D%5Bsize=11%5D%5Bcolor=#777%5DA%20SHIP%20IS%20SAFE%20IN%20HARBOR,%5B/color%5D%5B/size%5D%5B/b%5D%0A%5Bcolor=#276B91%5D%E2%96%8C%5B/color%5D%5Bb%5D%5Bsize=11%5D%5Bcolor=#777%5DBUT%20THAT'S%20NOT%20WHAT%20SHIPS%20ARE%20FOR.%5B/color%5D%5B/size%5D%5B/b%5D%0A%0A%0A%0A%5Balign=right%5D%5Bb%5DWelcome%20to%20%5Burl=http://bettergaia.com/%5DBetterGaia%5B/url%5D.%5B/b%5D%0A%5Bi%5DNeed%20help?%20%5Burl=http://www.gaiaonline.com/forum/t.45053993/%5DSee%20our%20thread.%5B/url%5D%5B/i%5D%5B/align%5D";
+        $(this).before('<format data-bbcode="' + bbcode + '" data-poststyle="0" draggable="true"><strong>Human</strong>\
+        <div class="clear"><a class="edit">Edit</a><a class="delete">Delete</a></div></format>');
+
+        $('#postformating aside').sortable('destroy').sortable({items: 'format:not(.add)'});
     });
 
     // Insert shortcuts
