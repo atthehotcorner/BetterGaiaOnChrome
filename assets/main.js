0194c43e0006ecb22a0a63d72e609ad707bf1fc2
atthehotcorner <at.thehotcorner@gmail.com>
2014-03-07 17:01:01 -0500

    Link

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         chrome.webstore.install('https://chrome.google.com/webstore/detail/lmgjagdflhhfjflolfalapokbplfldna', function() {
             $('#download a.button.chrome').parent().text('Get ready for a better gaia.');
         }, function() {
-            window.location.href = $('#download a.button.chrome').attr('href');
+            //window.location.href = $('#download a.button.chrome').attr('href');
         });
         return false;
     });
