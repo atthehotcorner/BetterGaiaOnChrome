96a76e9423331939172ea7289b29286481cd3899
Axel <at.thehotcorner@gmail.com>
2014-11-19 23:54:19 -0500

    test6

--- framework/extensions/core/script.js
+++ framework/extensions/core/script.js
@@ -6,9 +6,9 @@ Unauthorized copying, sharing, adaptation, publishing, commercial usage, and/or 
 
 $(document).ready(function() {
     $('#user_account').wrap('<div class="wrap"></div>');
-    $('#user_header_wrap > .hud-stats').css('margin-right', ($('#user_account').width() + 10) + 'px');
     $('#gaia_header #user_account').prepend('<a class="bgsettings">BG</a>');
     $('#gaia_header #user_account a.bgsettings').on('click', function() {
         window.location.hash = '#bgsettings';
     });
+    $('#user_header_wrap > .hud-stats').css('margin-right', ($('#user_account').width() + 10) + 'px');
 });
