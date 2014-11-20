499a7323788d6ccb43f95fb2938b2e6f45f912ac
Axel <at.thehotcorner@gmail.com>
2014-11-19 23:38:11 -0500

    test5

--- framework/extensions/core/script.js
+++ framework/extensions/core/script.js
@@ -4,8 +4,11 @@ Copyright (c) BetterGaia and Bowafishtech
 Unauthorized copying, sharing, adaptation, publishing, commercial usage, and/or distribution, its derivatives and/or successors, via any medium, is strictly prohibited.
 */
 
-
 $(document).ready(function() {
     $('#user_account').wrap('<div class="wrap"></div>');
-    $('#user_header_wrap > .hud-stats').css('margin-right', ($('#user_account').width() + 10) + 'px')
+    $('#user_header_wrap > .hud-stats').css('margin-right', ($('#user_account').width() + 10) + 'px');
+    $('#gaia_header #user_account').prepend('<a class="bgsettings">BG</a>');
+    $('#gaia_header #user_account a.bgsettings').on('click', function() {
+        window.location.hash = '#bgsettings';
+    });
 });
